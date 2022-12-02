06ee3a1ad08a83c75ea5942ca4c56d24df752963
MadLibberator <libbylord@hotmail.com>
2022-12-02 02:46:56 -0700

    Added consts for readability

--- Puzzles/Day02/Day2.cs
+++ Puzzles/Day02/Day2.cs
@@ -6,6 +6,10 @@ public class Day2 : Puzzle
 {
     private readonly Dictionary<string, int> _data = new();
 
+    private const char ROCK = 'X';
+    private const char PAPER = 'Y';
+    private const char SCISSORS = 'Z';
+
     public Day2(ILogger logger, string path) : base(logger, path) { }
 
     public override void Setup()
@@ -20,23 +24,21 @@ public class Day2 : Puzzle
     public override void SolvePart1()
     {
         var score = 0;
-
         foreach (var kvp in _data)
-            score += kvp.Value * (PointForThrowing(kvp.Key[2]) + PointForWinLossTie(kvp.Key));
-
+            score += kvp.Value * (PointsForThrowing(kvp.Key[2]) + PointsForResult(kvp.Key));
         _logger.Log(score);
     }
 
-    // X/Y/Z are our throws. A/B/C are opponent throws (not used, just to be thorough).
-    private static int PointForThrowing(char c) => c switch
+    // X/Y/Z are our throws. A/B/C are opponent throws (not used here).
+    private static int PointsForThrowing(char c) => c switch
     {
-        'A' or 'X' => 1, // Rock 
-        'B' or 'Y' => 2, // Paper
-        'C' or 'Z' => 3, // Scissors
+        ROCK => 1,
+        PAPER => 2,
+        SCISSORS => 3,
         _ => 0,
     };
 
-    private static int PointForWinLossTie(string matchup) => matchup switch
+    private static int PointsForResult(string matchup) => matchup switch
     {
         "A Z" or "B X" or "C Y" => 0, // Loss
         "A X" or "B Y" or "C Z" => 3, // Tie
@@ -47,23 +49,21 @@ public class Day2 : Puzzle
     public override void SolvePart2()
     {
         var score = 0;
-
         foreach (var kvp in _data)
-            score += kvp.Value * (PointForUpdatedOutcome(kvp.Key[2]) + PointForUpdatedThrow(kvp.Key));
-        
+            score += kvp.Value * (PointsForThrowing(UpdatedThrow(kvp.Key)) + PointsForKnownResult(kvp.Key[2]));
         _logger.Log(score);
     }
 
-    // X/Y/Z now means Loss/Tie/Win so we just remap the point values to match: 1/2/3 -> 0/3/6
-    private static int PointForUpdatedOutcome(char c) => 3 * (PointForThrowing(c) - 1);
-
     // Updated rules: X = need to lose, Y = tie, Z = win. This returns the value of what we need to *throw* to meet the condition
-    private static int PointForUpdatedThrow(string matchup) => matchup switch
+    private static char UpdatedThrow(string matchup) => matchup switch
     {
-        "A Y" or "B X" or "C Z" => 1, // We throw a Rock if we: want to Tie (Y) their Rock (A), Lose (X) to Paper (B), or Win (Z) against Scissors (C)
-        "A Z" or "B Y" or "C X" => 2, // Paper: Beats (Z) Rock (A), Ties (Y) Paper (B), Loses (X) to Scissors (C)
-        "A X" or "B Z" or "C Y" => 3, // Scissors: Loses (X) to Rock (A), Beats (Z) Paper (B), Ties (Y) Scissors (C)
-        _ => 0,
+        "A Y" or "B X" or "C Z" => ROCK, // Tie (Y) their Rock (A), Lose (X) to Paper (B), or Win (Z) against Scissors (C)
+        "A Z" or "B Y" or "C X" => PAPER, // Beats (Z) Rock (A), Ties (Y) Paper (B), Loses (X) to Scissors (C)
+        "A X" or "B Z" or "C Y" => SCISSORS, // Loses (X) to Rock (A), Beats (Z) Paper (B), Ties (Y) Scissors (C)
+        _ => '\0',
     };
+
+    // X/Y/Z now means Loss/Tie/Win so we just remap the point values to match: 1/2/3 -> 0/3/6
+    private static int PointsForKnownResult(char c) => 3 * (PointsForThrowing(c) - 1);
 }
 // nice

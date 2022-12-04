5ca06d5d92af18f8506cb6d2dc952a1319ffa079
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2022-12-04 19:57:42 +0100

    Solved Day 3

---
+++ Puzzles/Day03/Day3.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC22;
+
+public class Day3 : Puzzle
+{
+    private List<Sack> sacks = new();
+
+    public Day3(ILogger logger, string path) : base(logger, path) { }
+
+    public override void Setup()
+    {
+        foreach (var line in Utils.ReadFrom(_path))
+        {
+            sacks.Add(new Sack(
+                line[0..(line.Length / 2)],
+                line[(line.Length / 2)..^0]
+                ));
+        }
+    }
+
+    public override void SolvePart1()
+    {
+        int priority = 0;
+
+        foreach (var s in sacks)
+        {
+            var overlap = new HashSet<char>(s.Left);
+            overlap.IntersectWith(s.Right);
+
+            priority += overlap
+                .Select(c => (c >= 'A' && c <= 'Z') ? ((c - 'A') + 27) : ((c - 'a') + 1))
+                .Sum();
+        }
+
+        _logger.Log(priority);
+    }
+
+    public override void SolvePart2()
+    {
+        int priority = 0;
+
+        foreach (var s in sacks.Chunk(3))
+        {
+            var overlap = new HashSet<char>(s[0].Left + s[0].Right);
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                var sack = s[i];
+                overlap.IntersectWith(sack.Left + sack.Right);
+            }
+
+            if (overlap.Count != 1)
+                throw new Exception();
+
+            priority += overlap
+                .Select(c => (c >= 'A' && c <= 'Z') ? ((c - 'A') + 27) : ((c - 'a') + 1))
+                .Sum();
+        }
+
+        _logger.Log(priority);
+    }
+
+    private record Sack(string Left, string Right);
+}

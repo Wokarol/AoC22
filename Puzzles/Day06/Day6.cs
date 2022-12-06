71380e510539c85ae5ed076e2fbc521edb101fee
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2022-12-06 11:58:50 +0100

    Solved Day 6

---
+++ Puzzles/Day06/Day6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC22;
+
+public class Day6 : Puzzle
+{
+    string datastream = "";
+
+    public Day6(ILogger logger, string path) : base(logger, path) { }
+
+    public override void Setup()
+    {
+        datastream = Utils.ReadFrom(_path).First();
+    }
+
+    public override void SolvePart1()
+    {
+        int marker = 0;
+        const int markerLength = 4;
+        for (int i = markerLength; i <= datastream.Length; i++)
+        {
+            if (datastream[(i - markerLength)..i].Distinct().Count() == markerLength)
+            {
+                marker = i;
+                break;
+            }
+        }
+        _logger.Log(marker);
+    }
+
+    public override void SolvePart2()
+    {
+        int marker = 0;
+        const int markerLength = 14;
+        for (int i = markerLength; i <= datastream.Length; i++)
+        {
+            if (datastream[(i - markerLength)..i].Distinct().Count() == markerLength)
+            {
+                marker = i;
+                break;
+            }
+        }
+        _logger.Log(marker);
+    }
+}
